367bac1c052688050eee06071cee73aec1e86d93
Sgenmi <sgenmi@gmail.com>
2023-09-04 08:02:55 +0800

    add:getResponse,getSession

--- src/Application.php
+++ src/Application.php
@@ -8,11 +8,38 @@ declare(strict_types=1);
 
 namespace Weida\WeixinMiniProgram;
 
+use Psr\Http\Message\ResponseInterface;
 use Weida\WeixinCore\AbstractApplication;
 
 class Application extends AbstractApplication
 {
     protected string $appType='miniApp';
 
+    protected ?Session $session=null;
+
+
+    public function getResponse(): ResponseInterface
+    {
+        if(empty($this->response)){
+            $this->response = new Response(
+                $this->getRequest(),
+                $this->getEncryptor(),
+                $this->getAppType() //用来区别事件
+            );
+        }
+        return $this->response;
+    }
+
+    public function getSession():Session{
+        if(empty($this->session)){
+            $this->session = new Session(
+                appid: $this->getAccount()->getAppId(),
+                secret:$this->getAccount()->getSecret(),
+                accessToken:$this->getAccessToken(),
+                httpClient: $this->getHttpClient()
+            );
+        }
+        return $this->session;
+    }
 
 }
